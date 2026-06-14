f12e57e29007b58e143409da13c278caa1d988c1
Mouradmore <mouradx524@gmail.com>
2026-06-14 21:49:16 +0100

    Add files via upload

---
+++ My-FullStack-Project/Backend/models/Project.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+  author: { type: String, required: true },
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const ProjectSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  html: { type: String, default: "" },
+  css: { type: String, default: "" },
+  js: { type: String, default: "" },
+  author: { type: String, required: true },
+  views: { type: Number, default: 0 },
+  likes: { type: [String], default: [] }, // مصفوفة تحتوي على أسماء المستخدمين الذين وضعوا إعجاباً
+  comments: [CommentSchema]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Project', ProjectSchema);
